b6d79827fb7db489ecba476d347c8094ad7be870
Christopher.Martins <christopher.c.martins15@gmail.com>
2024-08-27 14:41:42 -0300

    Desenvolvimento do projeto ContaBanco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        /*
+         * int numero = 1021;
+         * String agencia = "067-8";
+         * String nomeCliente = "MARIO ANDRADE";
+         * double saldo = 237.48;
+         */
+
+        // Criação do metodo Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o nome de usuario:");
+        String nomeCliente = scanner.next();
+        
+        System.out.println("Por favor, digite o valor de deposito:");
+        double saldo = scanner.nextDouble();
+
+        // Mensagem de exibição para o usario
+        System.out.println("Olá" + " " + nomeCliente + "," + " obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é" + " " + "N°" + agencia + ".");
+        System.out.println("Conta" + " " + "N°" + numero + ".");
+        System.out.println("Seu saldo atual é de:" + " " + "R$" + saldo + ".");
+        System.out.println("Valor disponivel para saque:" + " " + "R$" + saldo + ".");
     }
 }
